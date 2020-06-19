4b928c1e158908284a6161534ed48e565f5416a3
anuj-nair <nairanuj29@gmail.com>
2020-06-19 23:31:06 +0530

    Cleared Few errors

--- js/register4B.js
+++ js/register4B.js
@@ -1,1 +1,14 @@
-const 
+// const det = document.getElementById("businessDet");
+// const form = document.getElementsById('form');
+// const username = document.getElementById("username");
+// const email = document.getElementById("email");
+// const passwd = document.getElementById("passwd");
+// const re_passwd = document.getElementById("re_passwd");
+
+// form.addEventListener('submit',(e)=>{
+//     let message = []
+//     if(name.value === '' || name.value == null){
+//         message.push('Name is required');
+//         name.style='border: 2px solid red'
+//     }
+// });
